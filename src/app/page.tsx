58982afd67a73a8344b9ecdc37d48e4d14a07c54
Dominic <dominicmartincruz@gmail.com>
2024-05-17 00:12:32 +0800

    updated text and icon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,17 @@
 import { LottieIcon } from "@/components/LottieIcon/LottieIcon";
-import HomepageIcon from "@/assets/lottie/hompage-icon.json";
+import HomepageIcon from "@/assets/lottie/brb-landing-animation.json";
 
 const Homepage = () => {
   return (
-    <div className={"h-screen w-full flex items-center justify-center"}>
-      <LottieIcon icon={HomepageIcon} height={500} />
+    <div
+      className={"h-screen w-full flex flex-col items-center justify-center"}
+    >
+      <div className="flex flex-col items-center col text-slate-700">
+        <h1 className="font-bold text-5xl">Be right back!</h1>
+        <h2 className="font-medium text-2xl">Drawing up something new.</h2>
+        <h2 className="self-end text-slate-500">-dom</h2>
+      </div>
+      <LottieIcon icon={HomepageIcon} height={750} />
     </div>
   );
 };
